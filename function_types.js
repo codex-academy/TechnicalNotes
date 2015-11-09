3b1a9deb57d25f8f2e82590f2a40118778fc5afa
avermeulen <andre.vermeulen@gmail.com>
2015-11-09 13:08:55 +0200

    changed the slides slightly

---
+++ function_types.js
@@ -0,0 +1,19 @@
+// https://developer.mozilla.org/en/docs/web/JavaScript/Reference/Operators/function
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function
+
+// function statement
+function sakkie(){
+    return "I am a shoprite sakkie"
+}
+
+console.log(sakkie());
+
+console.log(bakkie());
+
+// VS === ??
+
+// function expression
+var bakkie = function(){
+    return "I am a toyota bakkie";
+}
